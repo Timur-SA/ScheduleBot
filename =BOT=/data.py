d7cc291cc70b2d3437d943d0d18ee497379bb710
Ritme <tr12345678910s@gmail.com>
2025-07-12 10:28:47 +0300

    DB Import

---
+++ =BOT=/data.py
@@ -0,0 +1,28 @@
+import json
+from datetime import datetime, timedelta
+from sys import argv as _argv
+from os import path as _path
+
+localDir = _path.dirname(_argv[0])
+
+
+def loadData(name="global"):
+    with open(_path.join(localDir, "DataTables", f"{name}.json"), "r", encoding="utf-8") as jsonFile:
+        return json.load(jsonFile)
+
+def getDays(dataTable): 
+    for day in dataTable:
+        print(f"{day} - {[event for event in day]}")
+
+def getEventsbyDay(dataTable, delta = 0):
+    if(delta < 0): print("Runtime Warning: Delta < 0!")
+
+    eventsDay = datetime.now().date()
+    eventsDay += timedelta(delta)
+
+    for day in dataTable:
+        print(day)
+
+    #print(dataTable.get(eventsDay.strftime("%Y-%m-%d"), []))
+    events = dataTable.get("2025-07-13", [])
+    print(events)
--- =BOT=/handlers.py
+++ =BOT=/handlers.py
@@ -1,37 +1,38 @@
 from aiogram import F, Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
+import data
 router = Router()
 
 
-
 @router.message(CommandStart())
-async def start(message: Message):
-    await message.reply(f"Привет!")
+async def start(msg: Message):
+    await msg.reply(f"Привет!")
 
 
 @router.message(Command("today"))
-async def today(message: Message):
-    await message.reply(f"Привет!")
+async def today(msg: Message):
+    await msg.reply(f"Привет!")
+    data.getEventsbyDay(data.loadData())
 
 @router.message(Command("tomorrow"))
-async def tomorrow(message: Message):
-    await message.reply(f"Привет!")
+async def tomorrow(msg: Message):
+    await msg.reply(f"Привет!")
 
 @router.message(Command("week"))
-async def week(message: Message):
-    await message.reply(f"Привет!")
+async def week(msg: Message):
+    await msg.reply(f"Привет!")
 
 
 @router.message(Command("add"))
-async def add(message: Message):
-    await message.reply(f"Привет!")
+async def add(msg: Message, cmd : Command):
+    await msg.reply(f"Привет!")
 
 
 @router.message(Command("help"))
-async def help(message: Message):
-    await message.reply(f"Привет!")
+async def help(msg: Message):
+    await msg.reply(f"Привет!")
 
 @router.message(Command("schedule"))
-async def schedule(message: Message):
-    await message.reply(f"Привет!")
+async def schedule(msg: Message):
+    await msg.reply(f"Привет!")
